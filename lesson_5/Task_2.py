1f14a814e53125a647e4a27b3262c301d7c646f0
NikolayGlazkov <gn9377419582@gmail.com>
2023-03-31 17:55:44 +0300

    Task_2_end

--- lesson_5/Task_2.py
+++ lesson_5/Task_2.py
@@ -1,11 +1,16 @@
-num = str(int(input()))
-
-def chet_ne_chet (num):
-    chet = 0
-    nechet = 0
-    if int(num[-1:])%2 == 0:
-        chet+=1
+def numbers(num):
+    if len(num) == 0:
+        return (0, 0)
     else:
-        nechet +=1
-    chet_ne_chet(int(num[:-1])) 
-    print(chet,nechet)
+        # рекурсивный вызов для подстроки без последней цифры
+        chet, nechet = numbers(num[:-1])
+        if int(num[-1]) % 2 == 0:
+            chet += 1
+        else:
+            nechet += 1
+        # немогу понять как добавить в вывод строку перед значениями
+        return (chet, nechet)
+
+
+num = input("введите любои число :")
+print(numbers(num))
